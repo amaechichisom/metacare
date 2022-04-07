b36765a798fa60043390346f29886695954800e7
Amaechi Chisom <amaechilegend@gmail.com>
2022-04-07 15:15:35 +0100

    Publishing website

--- src/Pages/Dashboard/Components/Card/StaticCard.js
+++ src/Pages/Dashboard/Components/Card/StaticCard.js
@@ -7,9 +7,9 @@ function StaticCard(props) {
     <div className='grid grid-cols-1 lg:grid-cols-4 xl:grid-cols-5 border-border-color border-1 rounded-md mt-8 w-full'>
         
         <div className=' border-r-1 border-border-color lg:col-span-3 xl:col-span-4 p-4 '>
-            <div className='flex flex-col lg:flex-row justify-between items-start'>
-                <div className='flex justify-center items-center'>
-                    <h3 className='font-bold text-lg mr-3'>{props.title}</h3>
+            <div className='flex flex-col lg:flex-row justify-between items-center'>
+                <div className='flex justify-center  items-center'>
+                    <h3 className='font-bold text-base xl:text-lg mr-3 break-all whitespace-normal'>{props.title}</h3>
                     <span className='text-primary-green bg-primary-light-green p-2 py-1 rounded-md'>+4.14%</span>
                 </div>
 
@@ -40,7 +40,7 @@ function StaticCard(props) {
             </div>
 
             <div className='flex flex-col justify-center items-start w-3/5 lg:w-4/5 h-4/5 mx-auto pl-4 padding_card col-span-3 border-border-color border-1 rounded-md bg-border-background '>
-                <p className='text-sm text-body-black mb-2'>{props.cardBottomTitle}</p>
+                <p className='text-sm  text-body-black mb-2'>{props.cardBottomTitle}</p>
                 <h3 className='font-bold text-heading-black text-lg'>1 Hour 30 Mins</h3>
             </div>
 
