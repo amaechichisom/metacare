ea05a1914a88eaf40503e6386f7c5262e580cb99
Amaechi Chisom <amaechilegend@gmail.com>
2022-04-07 14:35:06 +0100

    Publishing website

--- src/Components/HOC/HOC.js
+++ src/Components/HOC/HOC.js
@@ -3,7 +3,7 @@ import './HOC.css'
 
 function HOC(props) {
     return (
-        <div className="px-2 md:px-4 xl:px-8 py-2 pb-32">
+        <div className="hoc_padding py-2 pb-32">
             {props.children}
         </div>
     )
